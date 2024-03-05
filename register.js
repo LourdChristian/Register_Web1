de817331dd42a259130f2bcce72b6eb463b989a7
Lourd Christian <Bautolourdz@gmail.com>
2024-03-05 16:12:35 +0800

    registers

---
+++ register.js
@@ -0,0 +1,27 @@
+console.log("Hello Boss!");
+console.error("ERROR HAPPENED!!!");
+console.warn("WARNING!!");
+
+// variables
+// string = "Wordss" , 'owowowowo', '123'
+// number = 123, 456e, 2398.12
+// boolean = true, false
+// object = {"apple", "banana"}, {1,2,3,4}
+// {"apple", 3, true}
+
+// type of
+console.log(typeof(["SUP_NINJA"]))
+
+let name = "Lourdz";
+ lastname = `Christian`,
+age = 69
+isProf = true;
+hobbies = ["WATCHING FANTASY","PLAYING GAMES"]
+run = function () {
+    console.log("Im running.");
+}
+console.log(name, age, isProf, hobbies);
+console.log(typeof run);
+console.table(hobbies);
+console.log("My name is " + name + " " + lastname);
+console.log (`My name is ${name} ${lastname}`);
